960faabf1b2bc47396957143030a8aa05ddc1da3
Ander Sola <tupanditta@gmail.com>
2025-12-09 19:44:08 +0100

    JUEGO_Wordle_Base.py
    He terminado la base. En futuros proyectos añadiré versiones del programa con un resultado más visual e interactivo.

--- JUEGO_Wordle_Base.py
+++ JUEGO_Wordle_Base.py
@@ -19,7 +19,7 @@ from random import randint
 
 #####       1. Iniciar Juego
 
-def escoger_palabra_aleatoria(): #TERMINADO
+def escoger_palabra_aleatoria(): 
     lista_palabras_posibles = ['P A N D A', 'J A M O N', 'A E R E O']
 
     num_aleatorio = randint(0, len(lista_palabras_posibles) - 1)
@@ -28,13 +28,13 @@ def escoger_palabra_aleatoria(): #TERMINADO
 
     return palabra_aleatoria #Devuelve una lista de caracteres
 
-def preguntar_palabra_usuario(N): #TERMINADO
+def preguntar_palabra_usuario(N): 
     palabra_usuario = input('Introduzca una palabra que contenga {} letras: '.format(N))
     palabra_usuario = palabra_usuario.upper()
 
     return palabra_usuario
 
-def palabra_valida_caracteres(palabra): #TERMINADO
+def palabra_valida_caracteres(palabra): 
     valida = True
     for letra in palabra:
         if not ('A' <= letra <= 'Z' or letra == 'Ñ'):
@@ -43,7 +43,7 @@ def palabra_valida_caracteres(palabra): #TERMINADO
 
     return valida
 
-def palabra_valida_longitud(palabra, N): #TERMINADO
+def palabra_valida_longitud(palabra, N): 
     valida = True
     if len(palabra) != N:
         valida = False
@@ -52,7 +52,7 @@ def palabra_valida_longitud(palabra, N): #TERMINADO
 
 #####       2. Palabra del Usuario VS Palabra Aleatoria
 
-def añadir_nueva_palabra(N): #TERMINADO
+def añadir_nueva_palabra(N): 
     while True:
         palabra_usuario = preguntar_palabra_usuario(N)
         if palabra_valida_caracteres(palabra_usuario) and palabra_valida_longitud(palabra_usuario, N):
@@ -60,23 +60,26 @@ def añadir_nueva_palabra(N): #TERMINADO
         
         if not palabra_valida_longitud(palabra_usuario, N):
             print() #Visual
-            print('La palabra debe contener {} lestras'.format(N))
-            
+            print('La palabra debe contener {} letras'.format(N))
+
         if not palabra_valida_caracteres(palabra_usuario):
             print() #Visual
             print('Algún caracter no es válido en _{}_'.format(palabra_usuario))
     
     return palabra_usuario
 
-def letra_correcta(palabra_aleatoria_espejo, nueva_palabra, lista_resultado):
+def coinciden_posiciones(palabra_aleatoria_espejo, nueva_palabra, lista_resultado):
     for posicion in range(len(nueva_palabra)):
         letra = nueva_palabra[posicion]
 
         if letra in palabra_aleatoria_espejo:
             if palabra_aleatoria_espejo[posicion] == letra:
                 lista_resultado[posicion] = letra.upper()
-                palabra_aleatoria_espejo[posicion] == None
+                palabra_aleatoria_espejo[posicion] = None
     
+    return palabra_aleatoria_espejo, lista_resultado
+
+def conciden_letras(palabra_aleatoria_espejo, nueva_palabra, lista_resultado):
     for posicion in range(len(nueva_palabra)):
         letra = nueva_palabra[posicion]
 
@@ -84,6 +87,13 @@ def letra_correcta(palabra_aleatoria_espejo, nueva_palabra, lista_resultado):
             if letra in palabra_aleatoria_espejo and letra is not None:
                 lista_resultado[posicion] = letra.lower()
                 palabra_aleatoria_espejo.remove(letra)
+
+    return lista_resultado
+
+def letra_correcta(palabra_aleatoria_espejo, nueva_palabra, lista_resultado):
+    palabra_aleatoria_espejo, lista_resultado = coinciden_posiciones(palabra_aleatoria_espejo, nueva_palabra, lista_resultado)
+    
+    lista_resultado = conciden_letras(palabra_aleatoria_espejo, nueva_palabra, lista_resultado)
     
     return lista_resultado
 
@@ -101,11 +111,13 @@ def mostrar_respuesta(palabra_aleatoria, nueva_palabra):
     
     print(lista_resultado)
 
-def comprobar_terminado(palabra_aleatoria, nueva_palabra):
+    return lista_resultado
+
+def comprobar_terminado(lista_resultado):
     terminado = True
 
-    for letra in nueva_palabra:
-        if letra not in palabra_aleatoria:
+    for letra in lista_resultado:
+        if not ('A' <= letra <= 'Z' or letra == 'Ñ'):
             terminado = False
             break
     
@@ -120,14 +132,15 @@ def wordle():
     terminado = False
 
     palabra_aleatoria = escoger_palabra_aleatoria()
+    print(palabra_aleatoria) #PANDA: borrar
 
     ###### TURNOS
     while not terminado:
         nueva_palabra = añadir_nueva_palabra(N)
 
-        mostrar_respuesta(palabra_aleatoria, nueva_palabra)
+        lista_resultado = mostrar_respuesta(palabra_aleatoria, nueva_palabra)
 
-        if comprobar_terminado(palabra_aleatoria, nueva_palabra):
+        if comprobar_terminado(lista_resultado):
             print() #Visual
             print('ENHORABUENA, HAS GANADO')
 
@@ -137,6 +150,4 @@ def wordle():
 ################################            JUGAR
 
 wordle()
-
-
 
